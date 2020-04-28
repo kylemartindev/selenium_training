5e3a2def4fbf4152349fe843f29e7e6be928f3d4
thenerdtopia <thenerdtopia2019@gmail.com>
2020-04-28 15:33:25 +0100

    Added chromedriver with before and after annotations

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,11 +1,25 @@
 package org.example;
 
 
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AppTest 
+public class AppTest
 {
+private WebDriver driver;
 
+@Before
+public void setup() {
 
+    driver = new ChromeDriver();
+}
+
+@After
+    public void teardown() {
+
+    driver.quit();
+}
 
 
 }
